bb0b25f926454e84fd1ad0f7e014932f83678ecc
chunmeng <chunmeng@yahoo.com>
2020-11-22 10:42:26 +0800

    Moves nvs init code out to make init flow cleaner

--- main/main.c
+++ main/main.c
@@ -9,15 +9,20 @@
 static const char ssid[] = "Gateway-SSID";
 static const char passphrase[] = "TestPass!23";
 
-void app_main(void)
+static esp_err_t nvs_init(void)
 {
-    /* initialize nvs partition on the system */
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK(ret);
+    return ret;
+}
+
+void app_main(void)
+{
+    /* initialize nvs partition on the system */
+    ESP_ERROR_CHECK(nvs_init());
     /* initialize TCP/IP stack */
     ESP_ERROR_CHECK(esp_netif_init());
     /* create the event loop */
